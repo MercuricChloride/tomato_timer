ad7902a7a6ceb90863f9e878b9fd6ac3989417d5
mercuricchloride <mercuryprotocol@protonmail.com>
2022-12-27 17:27:21 -0500

    break rounds added

--- src/app.rs
+++ src/app.rs
@@ -16,6 +16,7 @@ pub struct TemplateApp {
 
 struct Timer {
     time_per_round: f32,
+    time_per_break: f32,
     start_time: SystemTime,
     status: Option<TimerStatus>,
 }
@@ -23,7 +24,7 @@ struct Timer {
 #[derive(Debug)]
 enum TimerStatus {
     Running,
-    Break,
+    Break(SystemTime), // time when break started
     Stopped,
 }
 
@@ -31,9 +32,10 @@ impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             timer: Timer {
-                time_per_round: 0.0,
+                time_per_round: 25.0,
+                time_per_break: 5.0,
                 start_time: SystemTime::now(),
-                status: None,
+                status: Some(TimerStatus::Stopped),
             },
         }
     }
@@ -95,6 +97,7 @@ impl eframe::App for TemplateApp {
             timer:
                 Timer {
                     time_per_round,
+                    time_per_break,
                     start_time,
                     status,
                 },
@@ -109,56 +112,93 @@ impl eframe::App for TemplateApp {
             ui.separator();
 
             ui.add(egui::Slider::new(time_per_round, 0.0..=60.0).text("Minutes per round"));
+            ui.add(egui::Slider::new(time_per_break, 0.0..=60.0).text("Minutes per break"));
 
             let current_time = SystemTime::now();
 
-            let elapsed_time = current_time.duration_since(*start_time).unwrap();
-            let remaining_time = (*time_per_round * 60.0) - elapsed_time.as_secs_f32();
-
-            let button_text = match status {
-                Some(TimerStatus::Running | TimerStatus::Break) => "Stop Round",
-                _ => "Start Round",
+            let elapsed_time = match status {
+                Some(TimerStatus::Running) => current_time
+                    .duration_since(*start_time)
+                    .unwrap()
+                    .as_secs_f32(),
+                Some(TimerStatus::Break(start)) => {
+                    current_time.duration_since(*start).unwrap().as_secs_f32()
+                }
+                _ => 0.0,
             };
 
-            let time_remaining = match status {
-                Some(TimerStatus::Running) => {
-                    if remaining_time > 0.0 {
-                        Some(remaining_time)
-                    } else {
-                        None
-                    }
-                }
-                _ => None,
+            let remaining_time = match status {
+                Some(TimerStatus::Running) => (*time_per_round * 60.0) - elapsed_time,
+
+                Some(TimerStatus::Break(_)) => (*time_per_break * 60.0) - elapsed_time,
+
+                _ => 0.0,
+            };
+
+            let button_text = match status {
+                Some(TimerStatus::Running | TimerStatus::Break(_)) => "Stop Round",
+                _ => "Start Round",
             };
 
             // main timer logic and actions
             // if we're running, show the progress bar
-            if let Some(TimerStatus::Running) = status {
-                if remaining_time > 0.0 {
-                    ui.horizontal(|ui| {
-                        ui.add(egui::ProgressBar::new(remaining_time / *time_per_round));
-                        ui.label(format!(
-                            "{}",
-                            Duration::from_secs_f32(remaining_time).as_secs()
-                        ));
-                    });
-                } else {
-                    notifica::notify("Time is up!", "Take a break").unwrap();
-                    thread::spawn(|| {
-                        finish_sound();
-                    });
-                    *status = Some(TimerStatus::Stopped); // reset timer so we don't spam notifications
-                    ui.add(egui::ProgressBar::new(0.0));
+            match status {
+                Some(TimerStatus::Running) => {
+                    if remaining_time > 0.0 {
+                        ui.horizontal(|ui| {
+                            // ui.add(egui::ProgressBar::new(remaining_time / *time_per_round));
+                            ui.add(egui::ProgressBar::new(
+                                remaining_time / (*time_per_round * 60.0),
+                            ));
+                            ui.label(format!(
+                                "{}",
+                                Duration::from_secs_f32(remaining_time).as_secs()
+                            ));
+                        });
+                    } else {
+                        notifica::notify("Time is up!", "Take a break").unwrap();
+                        thread::spawn(|| {
+                            finish_sound();
+                        });
+                        *status = Some(TimerStatus::Break(current_time)); // reset timer so we don't spam notifications
+                        ui.add(egui::ProgressBar::new(0.0));
+                    }
                 }
+                Some(TimerStatus::Break(_)) => {
+                    if remaining_time > 0.0 {
+                        ui.horizontal(|ui| {
+                            ui.add(egui::ProgressBar::new(
+                                remaining_time / (*time_per_break * 60.0),
+                            ));
+                            ui.label(format!(
+                                "Seconds left in break: {}",
+                                Duration::from_secs_f32(remaining_time).as_secs()
+                            ));
+                        });
+                    } else {
+                        notifica::notify("Back to work!", "Start focusing again :)").unwrap();
+                        thread::spawn(|| {
+                            start_sound();
+                        });
+                        *status = Some(TimerStatus::Running); // reset timer so we don't spam notifications
+                        ui.add(egui::ProgressBar::new(0.0));
+                    }
+                }
+                _ => {}
             }
 
             // time remaining label
             ui.heading(
                 //rustfmt::skip
-                if let Some(remaining_time) = time_remaining {
+                if remaining_time > 60.0 {
                     format!(
-                        "Time remaining: {}",
-                        Duration::from_secs_f32(remaining_time).as_secs() / 60
+                        "{} Minutes left in round",
+                        (Duration::from_secs_f32(remaining_time).as_secs() / 60) + 1
+                    )
+                } else if remaining_time > 0.0 {
+                    format!(
+                        "{} Seconds left in round",
+                        Duration::from_secs_f32(remaining_time).as_secs()
                     )
                 } else {
                     "Time is up!".to_owned()
@@ -183,6 +223,7 @@ impl eframe::App for TemplateApp {
                     }
                 }
             }
+            ui.heading(format!("{:?}", status));
         });
     }
 }
